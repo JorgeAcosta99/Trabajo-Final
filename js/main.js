9aa322980eb696c38267223fa11bed2c0bf86145
Jorge <jorg3.acosta99@gmail.com>
2023-12-05 22:48:50 -0300

    a

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
-// Obtén el botón por su ID
+// Obtén los elementos del formulario y el mensaje de error por su ID
 var btnLogin = document.getElementById('btnLogin');
+var emailInput = document.getElementById('email');
+var passwordInput = document.getElementById('password');
+var errorMessage = document.getElementById('error-message');
 
 // Agrega un evento de clic al botón
 btnLogin.addEventListener('click', function() {
-    // Redirige al usuario a la siguiente página
-    window.location.href = '../pages/catalogo.html';
+    // Obtiene los valores del correo y la contraseña
+    var enteredEmail = emailInput.value;
+    var enteredPassword = passwordInput.value;
+
+    // Verifica si el correo y la contraseña son los predefinidos
+    if (enteredEmail === 'admin@gmail.com' && enteredPassword === 'admin') {
+        // Redirige al usuario a la siguiente página
+        window.location.href = '../pages/catalogo.html';
+    } else {
+        // Muestra un mensaje de error si el correo o la contraseña son incorrectos
+        errorMessage.textContent = 'Correo o contraseña incorrectos. Por favor, inténtalo de nuevo.';
+    }
 });

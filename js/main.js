74b6de8fb47371a021f4942ae0e2c450370ba504
Jorge <jorg3.acosta99@gmail.com>
2023-12-05 21:31:05 -0300

    a

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Obtén el botón por su ID
+var btnLogin = document.getElementById('btnLogin');
+
+// Agrega un evento de clic al botón
+btnLogin.addEventListener('click', function() {
+    // Redirige al usuario a la siguiente página
+    window.location.href = '../pages/catalogo.html';
+});
